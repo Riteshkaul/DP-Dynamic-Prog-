876f1799149d6bdcff37b720a74765088314aa25
Ritesh kaul <93066663+Riteshkaul@users.noreply.github.com>
2023-01-26 09:43:31 +0530

    Create Count of Subsets Sum with given sum.cpp

---
+++ Count of Subsets Sum with given sum.cpp
@@ -0,0 +1,35 @@
+	int perfectSum(int arr[], int n, int sum)
+	{
+        // Your code goes here
+        long long int dp[n+1][sum+1];
+        long long int mod=1e9+7;
+        for(int i=0;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(i==0)
+                {
+                    dp[i][j]=0;
+                }
+                if(j==0)
+                {
+                   dp[i][j]=1; 
+                }
+            }
+        }
+        for(int i=1;i<=n;i++)
+        {
+            for(int j=0;j<=sum;j++)
+            {
+                if(j>=arr[i-1])
+                {
+                    dp[i][j]=dp[i-1][j-arr[i-1]]%mod+dp[i-1][j]%mod;
+                }
+                else{
+                    dp[i][j]=dp[i-1][j]%mod;
+                }
+            }
+        }
+        return dp[n][sum]%mod;
+	}
+	  
